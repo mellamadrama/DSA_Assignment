718ec7149dddd257bd600b87e19dc738884503b8
KurvyHerv <hervsie@gmail.com>
2025-01-16 08:07:41 +0800

    updated vsproj

---
+++ DSA_Assignment/Admin.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include "Actor.h"
+#include "Movie.h"
+#include "HashTable.h"
+using namespace std;
+
+class Admin {
+private:
+    string name;
+    int adminId;
+};
+
